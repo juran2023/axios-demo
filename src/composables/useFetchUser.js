4734c797996c055523ab67b368e7529b2342249e
JuRan <juranfuture@gmail.com>
2026-01-02 00:27:04 +0800

    local storage cache data

--- src/composables/useFetchUser.js
+++ src/composables/useFetchUser.js
@@ -11,11 +11,19 @@ export function useFetchUser() {
     error.value = null;
     // use axios to fetch user data
 
+    let userData = localStorage.getItem("user");
+    if (userData) {
+      loading.value = false;
+      user.value = JSON.parse(userData);
+      return;
+    }
+
     try {
       const response = await axios.get(
         `http://localhost:8080/user?id=${userId}`
       );
       user.value = response.data;
+      localStorage.setItem("user", JSON.stringify(response.data));
     } catch (err) {
       error.value = err;
     } finally {
